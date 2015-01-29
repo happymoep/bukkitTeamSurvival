b450329f29be57d836d488cf2a8257fa179220ca
happymoep <happymoep@googlemail.com>
2015-01-29 14:23:40 +0100

    First part of the command handler

--- src/com/github/maride/bukkitTeamSurvival/BukkitTeamSurvival.java
+++ src/com/github/maride/bukkitTeamSurvival/BukkitTeamSurvival.java
@@ -1,12 +1,26 @@
 package com.github.maride.bukkitTeamSurvival;
 
+import java.util.Set;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
+import org.bukkit.scoreboard.Team;
 
 public class BukkitTeamSurvival extends JavaPlugin {
-
+	
+	ScoreboardManager manager = null;
+	Scoreboard board = null;
+	
 	public void onEnable(){ 
+		ScoreboardManager manager = Bukkit.getScoreboardManager();
+		Scoreboard board = manager.getNewScoreboard();
 		//System.out.println("BukkitTeamSurvival enabled."); //Discouraged as bukkit should do this automatically
 	}
 
@@ -30,46 +44,134 @@ public class BukkitTeamSurvival extends JavaPlugin {
 
 				if(args[1].equalsIgnoreCase("team")) {
 					if(args.length <= 2) {
-						sender.sendMessage("Usage: /ts setup team <add|remove|put|pull|color|list>");
+						sender.sendMessage("Usage: /ts setup team <create|delete|add|remove|color|list>");
 						return true;
 					}
 
-					if(args[2].equalsIgnoreCase("add")) {
+					if(args[2].equalsIgnoreCase("create")) {
 						if(args.length <= 3) {
-							sender.sendMessage("Usage: /ts setup team add <Teamname>");
+							sender.sendMessage("Usage: /ts setup team create <Teamname>");
 							return true;
 						}
-						sender.sendMessage("Adding " + args[3]);
+						Set<Team> teams = board.getTeams();
+						for(Team t : teams) {
+							if(t.getName() == args[3]) {
+								sender.sendMessage("Team " + args[3] + " exists already");
+								return true;
+						    }
+						}
+						board.registerNewTeam(args[3]);
+						sender.sendMessage("Team " + args[3] + " created.");
+						return true;
+					} else if(args[2].equalsIgnoreCase("delete")) {
+						if(args.length <= 3) {
+							sender.sendMessage("Usage: /ts setup team delete <Teamname>");
+							return true;
+						}
+						Team team = board.getTeam(args[3]);
+						if(team == null){
+							sender.sendMessage("There is no team named " + args[3]);
+							return true;
+						}
+						else{
+							team.unregister();
+							sender.sendMessage("Team " + args[3] + " deleted");
+							return true;
+						}
+					} else if(args[2].equalsIgnoreCase("add")) {
+						if(args.length <= 4) {
+							sender.sendMessage("Usage: /ts setup team add <Teamname> <Player> [Player2 [...]]");
+							return true;
+						}
+						Team team = board.getTeam(args[3]);
+						if(team == null){
+							sender.sendMessage("There is no team named " + args[3]);
+							return true;
+						}
+						String playersFound = "";
+						String playersNotFound = "";
+						for (int i = 4; i < args.length; i++) {
+							Player p = Bukkit.getPlayer(args[i]);
+							if(p == null) {
+								playersNotFound += " " + args[i];
+							}
+							else {
+								playersFound += " " + args[i];
+								team.addPlayer(p);
+							}
+						}
+						if(playersFound != "")
+							sender.sendMessage("Added" + playersFound + " to " + args[3]);
+						if(playersFound != "")
+							sender.sendMessage("Players not found:" + playersNotFound);
+						return true;
 					} else if(args[2].equalsIgnoreCase("remove")) {
-						if(args.length <= 3) {
-							sender.sendMessage("Usage: /ts setup team remove <Teamname>");
-							return true;
-						}
-						sender.sendMessage("Removing " + args[3]);
-					} else if(args[2].equalsIgnoreCase("put")) {
 						if(args.length <= 4) {
-							sender.sendMessage("Usage: /ts setup team put <Player> <Teamname>");
+							sender.sendMessage("Usage: /ts setup team remove <Teamname> <Player> [Player2 [...]]");
 							return true;
 						}
-						sender.sendMessage("Putting " + args[3] + " into " + args[4]);
-					} else if(args[2].equalsIgnoreCase("pull")) {
-						if(args.length <= 4) {
-							sender.sendMessage("Usage: /ts setup team pull <Player> <Teamname>");
+						Team team = board.getTeam(args[3]);
+						if(team == null){
+							sender.sendMessage("There is no team named " + args[3]);
 							return true;
 						}
-						sender.sendMessage("Pulling " + args[3] + " from " + args[4]);
+						String playersFound = "";
+						String playersNotFound = "";
+						Set<OfflinePlayer> players = team.getPlayers();
+						OfflinePlayer p = null;
+						for (int i = 4; i < args.length; i++) {
+							for(OfflinePlayer pi : players)
+								if(args[i] == pi.getName())
+									p = pi;
+							if(p == null) {
+								playersNotFound += " " + args[i];
+							}
+							else {
+								playersFound += " " + args[i];
+								team.removePlayer(p);
+							}
+						}
+						if(playersFound != "")
+							sender.sendMessage("Removed" + playersFound + " from " + args[3]);
+						if(playersFound != "")
+							sender.sendMessage("Players not found:" + playersNotFound);
+						return true;
 					} else if(args[2].equalsIgnoreCase("color")) {
 						if(args.length <= 4) {
-							sender.sendMessage("Usage: /ts setup team color <color> <Teamname>");
+							sender.sendMessage("Usage: /ts setup team color <Teamname> <color>");
 							return true;
 						}
-						sender.sendMessage("Coloring " + args[4] + " in " + args[3]);
+						Team team = board.getTeam(args[3]);
+						if(team == null){
+							sender.sendMessage("There is no team named " + args[3]);
+							return true;
+						}
+						ChatColor color = ChatColor.valueOf(args[4]);
+						//TODO: Set the teams color
+						sender.sendMessage("Coloring " + args[3] + " in " + color.toString());
+						return true;
 					} else if(args[2].equalsIgnoreCase("list")) {
 						if(args.length <= 3) {
 							sender.sendMessage("Usage: /ts setup team list <Teamname>");
 							return true;
 						}
-						sender.sendMessage("Listing players of team " + args[3]);
+						Team team = board.getTeam(args[3]);
+						if(team == null){
+							sender.sendMessage("There is no team named " + args[3]);
+							return true;
+						}
+						Set<OfflinePlayer> players = team.getPlayers();
+						if(players.isEmpty()) {
+							sender.sendMessage("Team " + args[3] + " has no members");
+							return true;
+						}
+						String names = "";
+						for(OfflinePlayer p : players)
+							names += ", " + p.getName();
+						names = names.replaceFirst(", ", "");
+						sender.sendMessage("Members of " + team.getName() + ":");
+						sender.sendMessage(names);
+						return true;
 					} else {
 						sender.sendMessage("ts: No such team command: " + args[2]);
 					}
