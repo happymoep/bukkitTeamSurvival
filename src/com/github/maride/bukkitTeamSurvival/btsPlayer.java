e610e4e9630f03e7f0d546c179caa1734a23b387
maride <martin.dessauer@t-online.de>
2015-02-01 21:19:16 +0100

    Follow java name conventions

--- src/com/github/maride/bukkitTeamSurvival/btsPlayer.java
+++ src/com/github/maride/bukkitTeamSurvival/btsPlayer.java
@@ -1,27 +1,27 @@
 package com.github.maride.bukkitTeamSurvival;
 
-import com.github.maride.bukkitTeamSurvival.btsTeam;
+import com.github.maride.bukkitTeamSurvival.BTSTeam;
 import org.bukkit.entity.Player;
 
-public class btsPlayer {
-	btsTeam team;
+public class BTSPlayer {
+	BTSTeam team;
 	private String name;
 	private Player entity;
 
-	public btsPlayer(String playerName) {
+	public BTSPlayer(String playerName) {
 		// this(Server.getPlayer(playerName));
 	}
 	
-	public btsPlayer(String playerByName,btsTeam playerTeam) {
+	public BTSPlayer(String playerByName,BTSTeam playerTeam) {
 		// this(Server.getPlayer(playerName),playerTeam);
 	}
 	
-	public btsPlayer(Player playerByEntity) {
+	public BTSPlayer(Player playerByEntity) {
 		name = playerByEntity.getDisplayName();
 		entity = playerByEntity;
 	}
 	
-	public btsPlayer(Player playerByEntity,btsTeam playerTeam) {
+	public BTSPlayer(Player playerByEntity,BTSTeam playerTeam) {
 		this(playerByEntity);
 		team = playerTeam;
 	}
--- src/com/github/maride/bukkitTeamSurvival/btsTeam.java
+++ src/com/github/maride/bukkitTeamSurvival/btsTeam.java
@@ -1,11 +1,11 @@
 package com.github.maride.bukkitTeamSurvival;
 
-import com.github.maride.bukkitTeamSurvival.btsPlayer;
+import com.github.maride.bukkitTeamSurvival.BTSPlayer;
 
-public class btsTeam {
-	btsPlayer[] members;
+public class BTSTeam {
+	BTSPlayer[] members;
 	
-	public void addPlayer(btsPlayer thisPlayer) {
+	public void addPlayer(BTSPlayer thisPlayer) {
 		if(thisPlayer.team != null) {
 			// already got a team
 			thisPlayer.team.removePlayer(thisPlayer);
@@ -14,7 +14,7 @@ public class btsTeam {
 		// add to members
 	}
 	
-	public void removePlayer(btsPlayer thisPlayer) {
+	public void removePlayer(BTSPlayer thisPlayer) {
 		// remove from members
 	}
 }
